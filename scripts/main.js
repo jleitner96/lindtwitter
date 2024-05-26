074e4a8debc733325b6ddcd34557d94efa8ca36f
jleitner96 <149582267+jleitner96@users.noreply.github.com>
2024-05-26 12:33:21 +0200

    Delete scripts/main.js

--- scripts/main.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.button-container').forEach(container => {
-    container.addEventListener('click', event => {
-        if (event.target.closest('.heart-button')) {
-            // Toggle the 'clicked' class on the heart button
-            event.target.closest('.heart-button').classList.toggle('clicked');
-        }
-    });
-});
